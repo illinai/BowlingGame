1251c116d4e1509f365623d699b24bc302427244
illinai <144272615+illinai@users.noreply.github.com>
2025-02-19 02:01:32 -0700

    launch indicator

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -6,6 +6,7 @@ public class BallController : MonoBehaviour
     [SerializeField] private float force = 1f;
     [SerializeField] private InputManager inputManager;
     [SerializeField] private Transform ballAnchor;
+    [SerializeField] private Transform launchIndicator;
 
     private Rigidbody ballRB;
     private bool isBallLaunched;
@@ -35,7 +36,8 @@ public class BallController : MonoBehaviour
         transform.parent = null;
         ballRB.isKinematic = false;
 
-        ballRB.AddForce(transform.forward * force, ForceMode.Impulse);
+        ballRB.AddForce(launchIndicator.forward * force, ForceMode.Impulse);
+        launchIndicator.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
