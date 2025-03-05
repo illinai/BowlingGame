c8f025478601e5bd03d0e8f6a9fbafe0d593bdae
illinai <144272615+illinai@users.noreply.github.com>
2025-03-05 05:44:09 -0800

    assets

--- Assets/FallTrigger.cs
+++ Assets/FallTrigger.cs
@@ -13,18 +13,6 @@ public class FallTrigger : MonoBehaviour
             isPinFallen = true;
             OnPinFall?.Invoke();
             Debug.Log($"{gameObject.name} is fallen");
-        }
-        
-    }
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        }   
     }
 }
